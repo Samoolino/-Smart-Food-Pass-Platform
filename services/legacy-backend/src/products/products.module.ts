53989fe18bad875c87d5e292e75ff56fcd850013
Samoolino <samo4au@gmail.com>
2026-04-22 22:03:19 +0200

    build: wire products module

--- services/legacy-backend/src/products/products.module.ts
+++ services/legacy-backend/src/products/products.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pass } from '../passes/entities/pass.entity';
+import { Product } from './entities/product.entity';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 @Module({
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([Product, Pass])],
+  controllers: [ProductsController],
+  providers: [ProductsService],
 })
 export class ProductsModule {}
