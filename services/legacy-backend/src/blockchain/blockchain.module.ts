10031938d1645b4bbca057104f5ea95add7c6b9a
Samoolino <samo4au@gmail.com>
2026-04-22 22:31:27 +0200

    build: wire blockchain module

--- services/legacy-backend/src/blockchain/blockchain.module.ts
+++ services/legacy-backend/src/blockchain/blockchain.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BlockchainController } from './blockchain.controller';
+import { BlockchainService } from './blockchain.service';
 
 @Module({
-  controllers: [],
-  providers: [],
+  controllers: [BlockchainController],
+  providers: [BlockchainService],
+  exports: [BlockchainService],
 })
 export class BlockchainModule {}
