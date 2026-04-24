e842341acde40ef481deaf17f748ef3b82b757cb
Samoolino <samo4au@gmail.com>
2026-04-24 02:55:00 +0200

    feat: register merchant registry and plan entities in products module

--- services/legacy-backend/src/products/products.module.ts
+++ services/legacy-backend/src/products/products.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Merchant } from '../merchants/entities/merchant.entity';
 import { Pass } from '../passes/entities/pass.entity';
+import { UserPlan } from '../payments/entities/user-plan.entity';
+import { MerchantProductRegistry } from './entities/merchant-product-registry.entity';
 import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Pass])],
+  imports: [TypeOrmModule.forFeature([Product, Pass, Merchant, MerchantProductRegistry, UserPlan])],
   controllers: [ProductsController],
   providers: [ProductsService],
+  exports: [ProductsService, TypeOrmModule],
 })
 export class ProductsModule {}
