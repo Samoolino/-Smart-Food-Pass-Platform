55014ab0eee50a9cb84ff53e39c014c3ec04786c
Samoolino <samo4au@gmail.com>
2026-04-24 05:45:04 +0200

    build: register onboarding draft entity in users module

--- services/legacy-backend/src/users/users.module.ts
+++ services/legacy-backend/src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { OnboardingDraft } from './entities/onboarding-draft.entity';
 import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, OnboardingDraft])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService, TypeOrmModule],
